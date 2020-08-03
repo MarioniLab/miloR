3bfe70b9209e09ca7b8483db8bee68eb718bf060
MikeDMorgan <michael.morgan@cruk.cam.ac.uk>
2020-08-03 14:41:55 +0100

    add generics

---
+++ R/AllGenerics.R
@@ -0,0 +1,25 @@
+
+#' @export
+setGeneric("graph", function(x) standardGeneric("graph"))
+
+#' @export
+setGeneric("graph<-", function(x, value) standardGeneric("graph<-"))
+
+#' @export
+setGeneric("neighbourDistances", function(x) standardGeneric("neighbourDistances"))
+
+#' @export
+setGeneric("neighbourDistances<-", function(x, value) standardGeneric("neighbourDistances<-"))
+
+#' @export
+setGeneric("neighbourhoods", function(x) standardGeneric("neighbourhoods"))
+
+#' @export
+setGeneric("neighbourhoods<-", function(x, value) standardGeneric("neighbourhoods<-"))
+
+
+#' @export
+setGeneric("neighbourhoodCounts", function(x) standardGeneric("neighbourhoodCounts"))
+
+#' @export
+setGeneric("neighbourhoodCounts<-", function(x, value) standardGeneric("neighbourhoodCounts<-"))
