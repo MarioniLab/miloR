f50c2ada93dc3a269d8058d0c03452082f2754cb
Emma Dann <ed6@sanger.ac.uk>
2020-08-04 14:27:50 +0100

    Adding fast neighborhood function

--- R/makeNeighbourhoods.R
+++ R/makeNeighbourhoods.R
@@ -128,3 +128,137 @@ refine_vertex <- function(vertex.knn, v.ix, X_pca){
     refined.vertex <- findKNN(rbind(v.med, X_pca), subset=1, k=1)[["index"]][1] - 1 ## -1 to remove the median
     return(refined.vertex)
 }
+
+#' Define neighbourhoods on a graph (fast)
+#'
+#' This function randomly samples vertcies on a graph to define neighbourhoods.
+#' These are then refined by computing the median profile for the neighbourhood
+#' in reduced dimensional space and selecting the nearest vertex to this
+#' position. Thus, multiple neighbourhoods may be collapsed down together to
+#' prevent over-sampling the graph space.
+#' @param x A \code{\linkS4class{Milo}} object with a non-empty \code{graph}
+#' slot. Alternatively an \code{igraph} object on which neighbourhoods will
+#' be defined.
+#' @param prop A double scalar that defines what proportion of graph vertices
+#' to randomly sample. Must be 0 < prop < 1.
+#' @param k An integer scalar - the same k used to construct the input graph.
+#' @param reduced_dims If x is an \code{\linkS4class{Milo}} object, a character indicating the name of the \code{reducedDim} slot in the 
+#' \code{\linkS4class{Milo}} object to use as (default: 'PCA'). If x is an \code{igraph} object, a 
+#' matrix of vertices X reduced dimensions.
+#' @param seed An integer scalar seed to initial the pseudorandom number
+#' generator
+#' @details
+#' This function randomly samples graph vertices, then refines them to collapse
+#' down the number of neighbourhoods to be tested. The refinement behaviour can
+#' be turned off by setting \code{refine=FALSE}, however, we do not recommend
+#' this as neighbourhoods will contain a lot of redundancy and lead to an
+#' unncecessarily larger multiple-testing burden.
+#'
+#' @return A \code{\linkS4class{Milo}} object containing a list of vertices and
+#' the indices of vertices that constitute the neighbourhoods in the
+#' neighbourhoods slot. If the input is a \code{igraph} object then the output
+#' is a list of vertices and the indices of vertices that constitute the
+#' neighbourhoods.
+#'
+#' @author
+#' Emma Dann, Mike Morgan
+#'
+#' @examples
+#'
+#' requires(igraph)
+#' m <- matrix(rnorm(10000), ncol=10)
+#' milo <- buildGraph(m, d=10)
+#'
+#' milo <- makeNeighbourhoodsFast(milo, prop=0.1)
+#' milo
+#' @name makeNeighbourhoodsFast
+makeNeighbourhoodsFast <- function(x, prop=0.1, k=21, refined=TRUE, seed=42, reduced_dims="PCA", k2=100) {
+    if(class(x) == "Milo"){
+        message("Checking valid object")
+        # check that a graph has been built
+        if(!.valid_graph(miloR::graph(x))){ ## Had to specify here for conflict w igraph
+            stop("Not a valid Milo object - graph is missing. Please run buildGraph() first.")
+        }
+        graph <- miloR::graph(x)
+        X_reduced_dims  <- reducedDim(x, reduced_dims)
+        if (d > ncol(X_reduced_dims)) {
+            warning(paste("Warning: specified n_components is higher than the total number of dimensions in reducedDim(x, reduced_dims). Falling back to using", ncol(X_reduced_dims),"components\n"))
+            d <- ncol(X_reduced_dims)
+        }
+        X_reduced_dims  <- X_reduced_dims[,1:d]
+    } else if(class(x) == "igraph"){
+        if(!is.matrix(reduced_dims) & isTRUE(refined)){
+            stop("No reduced dimensions matrix provided - required for refined sampling")
+        }
+        graph <- x
+        X_reduced_dims <- reduced_dims
+    } else{
+        stop(paste0("Data format: ", class(x), " not recognised. Should be Milo or igraph"))
+    }
+    random_vertices <- .sample_vertices(graph, prop, seed)
+    if (isFALSE(refined)) {
+        sampled_vertices <- random_vertices
+    } else if (isTRUE(refined)) {
+        vertex.knn <-
+            findKNN(
+                X = X_reduced_dims,
+                k = k,
+                subset = as.vector(random_vertices),
+                get.index = TRUE,
+                get.distance = FALSE
+            )
+        knn_mat <-
+            matrix(0, nrow = length(as.vector(random_vertices)), ncol = ncol(x))
+        knn_mat <- as(knn_mat, "sparseMatrix")
+        for (ix in 1:nrow(knn_mat)) {
+            knn_mat[ix, vertex.knn$index[ix, ]] <- 1
+        }
+        ## Calculate avg profile of nearest neighbors
+        nh_reduced_dims <- knn_mat %*% X_reduced_dims
+        nh_reduced_dims <- t(apply(
+            nh_reduced_dims,
+            1,
+            FUN = function(x)
+                x / k
+        ))
+        rownames(nh_reduced_dims) <- paste0('nh_', 1:nrow(nh_reduced_dims))
+        
+        ## Search nearest cell to average profile
+        # I have to do this trick because as far as I know there is no fast function to 
+        # search for NN between 2 distinct sets of points (here I'd like to search NNs of 
+        # nh_reduced_dims points among X_reduced_dims points). Suggestions are welcome
+        all_reduced_dims <- rbind(nh_reduced_dims, X_reduced_dims)
+        nn_mat <- BiocNeighbors::findKNN(all_reduced_dims,
+                                         k = k2,
+                                         subset = rownames(nh_reduced_dims))[["index"]]
+        nn_mat_names <- apply(nn_mat, c(1,2), function(x) rownames(all_reduced_dims)[x])
+        i = 1
+        sampled_vertices <- rep("nh_0", nrow(nn_mat))
+        while (any(grepl(sampled_vertices, pattern = "nh_"))) {
+            update_ix <- grep(sampled_vertices, pattern="nh_")
+            sampled_vertices[update_ix] <- nn_mat_names[update_ix, i]
+            i <- i + 1
+            if (i > k2) {
+                stop("Average profiles are closer to each other than to real cells. Try increasing k2.")
+            }
+        }
+        sampled_vertices <- match( sampled_vertices, rownames(X_reduced_dims))
+    } 
+    
+    sampled_vertices <- unique(sampled_vertices)
+    nh_list <-
+        sapply(
+            1:length(sampled_vertices),
+            FUN = function(X)
+                neighbors(graph, v = sampled_vertices[X])
+        )
+    nh_list <- setNames(nh_list, sampled_vertices)
+    if(class(x) == "Milo"){ 
+        neighbourhoods(x) <- nh_list 
+        return(x)
+    } else {
+        return(nh_list)
+    }
+}
+
+
