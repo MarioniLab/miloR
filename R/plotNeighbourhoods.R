e1d4ee3a87ef7ea98856d545cfcee91e7fb73805
Emma Dann <ed6@sanger.ac.uk>
2020-08-06 15:39:47 +0100

    Adding function to plot DA test results on single-cell embedding

--- R/plotNeighbourhoods.R
+++ R/plotNeighbourhoods.R
@@ -1,4 +1,8 @@
-### Plotting utils for neighbourhoods
+###########################
+### MILO PLOTTING UTILS ###
+###########################
+
+### Plotting neighbourhoods stats ###
 
 #' Plot histogram of neighbourhood sizes
 #' 
@@ -50,3 +54,102 @@ plotNeighborhoodSizeHist <- function(milo, bins=50){
     FALSE
   }
 }
+
+### Plotting DA test results ###
+
+#' Plot Milo test results on reduced dimensiona
+#' 
+#' Visualize log-FC estimated with differential neighbourhood abundance testing
+#' on embedding of original single-cell dataset. 
+#'
+#' @param x A \code{\linkS4class{Milo}} object 
+#' @param milo_results A `data.frame` containing the results of differential neighbourhood abundance testing (output of \code{testNeighborhoods})
+#' --> this will need to be changed/removed when output of testNeighbourhoods changes
+#' @param reduced_dims a character indicating the name of the \code{reducedDim} slot in the 
+#' \code{\linkS4class{Milo}} object to use as (default: 'UMAP').
+#' @param filter_alpha the spatialFDR cutoff used as a significance threshold. If not \code{NULL} the logFC will be plotted only for 
+#' significantly DA neighbourhoods (default: NULL)
+#' @param pt_size size of scatterplot points (default: 1.5)
+#' @param components vector of reduced dimensions components to plot (default: c(1,2))
+#' 
+#' @return a \code{\linkS4class{ggplot}} object
+#' 
+#' @author Emma Dann
+#' 
+#' @examples 
+#' NULL
+#' 
+#' @export
+#' @rdname plotMiloReducedDim
+#' @import ggplot2
+#' @importFrom dplyr left_join mutate arrange
+plotMiloReducedDim <- function(x, milo_results, reduced_dims="UMAP", filter_alpha=NULL, 
+                               pt_size=1.5, components=c(1,2)
+){
+  ## Join test results and dimensionality reductions
+  if (reduced_dims %in% reducedDimNames(x)){
+    rdim_df <- data.frame(reducedDim(x, reduced_dims))[,components]
+  } else {
+    stop(paste(reduced_dims, "is not the name of a dimensionality reduction result in x. Available reductions are:", paste(reducedDimNames(x), collapse = ", ")))
+  }
+  colnames(rdim_df) <- c('X','Y')
+  rdim_df[,"nhIndex"] <- 1:nrow(rdim_df)
+  viz2_df  <- left_join(rdim_df, milo_results, by="nhIndex") 
+  
+  ## Filter significant DA neighbourhoods 
+  if (!is.null(filter_alpha)) {
+    viz2_df <- mutate(viz2_df, logFC = ifelse(SpatialFDR > filter_alpha, NA, logFC)) 
+  }
+  
+  ## Plot 
+  pl <-
+    ggplot(data = arrange(viz2_df, abs(logFC)),
+           aes(X, Y)) +
+    geom_point(aes(color = ''), size = pt_size / 3, alpha = 0.5) +
+    geom_point(
+      data = . %>% filter(!is.na(SpatialFDR)),
+      aes(fill = logFC),
+      size = pt_size,
+      stroke = 0.1,
+      # colour="black",
+      shape = 21
+    ) +
+    scale_fill_gradient2(
+      midpoint = 0,
+      high = "red",
+      low = "blue",
+      name = "log-FC"
+    ) +
+    xlab(paste(reduced_dims, components[1], sep="_")) +
+    ylab(paste(reduced_dims, components[2], sep="_"))
+  
+  if (!is.null(filter_alpha)) {
+    pl <- pl +
+      scale_color_manual(values = 'grey', label = paste("SpatialFDR <", round(filter_alpha, 2))) +
+      guides(colour = guide_legend(
+        '',
+        override.aes = list(
+          shape = 21,
+          colour = "black",
+          fill = "grey50",
+          size = pt_size,
+          alpha = 1,
+          stroke = 0.1
+        )
+      ))
+  } else {
+    pl <- pl + 
+      scale_color_manual(values = 'grey') +
+      guides(color="none")
+  }
+  
+  pl <- pl +
+    theme_classic(base_size = 16) +
+    theme(
+      axis.ticks = element_blank(),
+      axis.text = element_blank(),
+      plot.title = element_text(hjust = 0.5)
+    )
+  return(pl)
+}
+
