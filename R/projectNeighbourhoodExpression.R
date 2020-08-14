f2acd75636a3398c022871ffdb91b5d9a8d639ac
Emma Dann <ed6@sanger.ac.uk>
2020-08-14 11:59:01 +0100

    Fixing naming conflict

--- R/projectNeighbourhoodExpression.R
+++ R/projectNeighbourhoodExpression.R
@@ -1,30 +1,30 @@
-#' Project median neighbourhood expression profiles on embedding of single-cells
+#' Project mean nhood expression profiles on embedding of single-cells
 #' 
-#' This is for visualization of single-cells and neighbourhoods on the same embedding
+#' This is for visualization of single-cells and nhoods on the same embedding
 #' 
-#' @param x A \code{\linkS4class{Milo}} object with a non-empty \code{neighbourhoods}
+#' @param x A \code{\linkS4class{Milo}} object with a non-empty \code{nhoods}
 #' slot. 
 #' @param d The number of reduced dimensions to use
 #' @param reduced_dims a character indicating the name of the \code{reducedDim} slot in the
 #'  \code{\linkS4class{Milo}} object to use  for projection (default: 'PCA'). \code{reducedDim(x,reduced_dim)}
 #'  needs to have the attribute \code{rotation} storing the gene X dimensions loading matrix to use for projection.
 #'  See details for more info.
-#' @param scale logical indicating whether neighbourhoods expression profiles should be scaled before projection. 
+#' @param scale logical indicating whether nhoods expression profiles should be scaled before projection. 
 #' Data should be scaled if the embedding was performed on scaled single-cell data. 
 #' (Default: TRUE)
-#' @param center logical indicating whether neighbourhoods expression profiles should be centered before projection
+#' @param center logical indicating whether nhoods expression profiles should be centered before projection
 #' Data should be centered if the embedding was performed on scaled single-cell data.
 #' (Default: TRUE)
 #'
 #' @details
-#' This function projects neighbourhoods in the same reduced dimensions used for the full single-cell dataset.
-#' First it calculates the median expression profile for all cells in a neighbourhood. Then the gene X nhood matrix is 
+#' This function projects nhoods in the same reduced dimensions used for the full single-cell dataset.
+#' First it calculates the mean expression profile for all cells in a nhood. Then the gene X nhood matrix is 
 #' multiplied by the matrix of loadings of genes on reduced dimensions. The loading matrix should be stored as an attribute \code{rotation}
 #' in \code{reducedDim(x, reduced_dims)}. This can be added by running \code{runPCA(x)} from the \code{scater} package or, using the
 #' package \code{irlba} as in examples. The output of this function is used for visualization of test results.
 #'
 #' @return A \code{\linkS4class{matrix}} object of samples X reduced dimensions where samples are both
-#' the single-cells and the neighbourhoods.
+#' the single-cells and the nhoods.
 #' (This will need to become a slot in Milo object)
 #' 
 #' @author
@@ -36,17 +36,17 @@
 #' m <- matrix(rnorm(100000), ncol=100)
 #' milo <- Milo(SingleCellExperiment(assays(logcounts=m)))
 #' milo <- buildGraph(m, d=30, transposed=TRUE)
-#' milo <- makeNeighbourhoods(milo)
-#' milo <- projectNeighbourhoodExpression(milo)
+#' milo <- makeNhoods(milo)
+#' milo <- projectNhoodExpression(milo)
 #' 
 #' @export
-#' @rdname projectNeighbourhoodExpression
-projectNeighbourhoodExpression <- function(x, d = 30, reduced_dims = "PCA", scale=TRUE){
+#' @rdname projectNhoodExpression
+projectNhoodExpression <- function(x, d = 30, reduced_dims = "PCA", scale=TRUE){
   if (class(x) != "Milo") {
     stop("Unrecognised input type - must be of class Milo")
-  } else if (!isTRUE(.valid_neighbourhood(x))) {
+  } else if (!isTRUE(.valid_nhood(x))) {
     stop(
-      "Not a valid Milo object - neighbourhoods are missing. Please run makeNeighbourhoods() first."
+      "Not a valid Milo object - nhoods are missing. Please run makeNhoods() first."
     )
   }
   
@@ -64,26 +64,26 @@ projectNeighbourhoodExpression <- function(x, d = 30, reduced_dims = "PCA", scal
   
   if (is.null(attr(reducedDim(x), "rotation"))) {
     stop(
-      "to project neighbourhoods on single-cell embedding the loading matrix for 'reduced_dim' needs to be stored as attribute 'rotation' in reducedDim(x,reduced_dim)."
+      "to project nhoods on single-cell embedding the loading matrix for 'reduced_dim' needs to be stored as attribute 'rotation' in reducedDim(x,reduced_dim)."
     )
   }
   
-  ## Calculate mean profile of cells in a neighbourhood
-  if (is.null(neighbourhoodExpression(sim_milo))) {
-    x <- calcNeighbourhoodExpression(x, assay = "logcounts")
+  ## Calculate mean profile of cells in a nhood
+  if (is.null(nhoodExpression(sim_milo))) {
+    x <- calcNhoodExpression(x, assay = "logcounts")
   }
   
-  ## Scale the neighbourhoods profile (if PCs were calculated on scaled data)
+  ## Scale the nhoods profile (crucial if PCs were calculated on scaled data)
   if (isTRUE(scale) | isTRUE(center)) {
-    neighbourhoodExpression(x) <- t(scale(t(neighbourhoodExpression(x)), scale=TRUE, center=TRUE))
+    nhoodExpression(x) <- t(scale(t(nhoodExpression(x)), scale=TRUE, center=TRUE))
   }
   
   ## Project profiles to same PCA space of the single-cells
   X_reduced_dims <- reducedDim(x, reduced_dims)
   loadings <- attr(X_reduced_dims, "rotation")
-  n.reducedDim <- t(neighbourhoodExpression(x)) %*% loadings
+  n.reducedDim <- t(nhoodExpression(x)) %*% loadings
   
-  ## Make one PC matrix including single-cells and neighbourhoods
+  ## Make one PC matrix including single-cells and nhoods
   rownames(n.reducedDim) <-
     paste0("nh_", seq(1:nrow(n.reducedDim)))
   X_reduced_dims_merged <- rbind(n.reducedDim, X_reduced_dims)
@@ -108,3 +108,4 @@ projectNeighbourhoodExpression <- function(x, d = 30, reduced_dims = "PCA", scal
   }
   return(a)
 }
+
