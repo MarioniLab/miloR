7d6dd869034decaa01da76fe01968f91da86bd90
MikeDMorgan <michael.morgan@cruk.cam.ac.uk>
2021-05-05 11:54:43 +0100

    add package help page

---
+++ R/miloR-package.R
@@ -0,0 +1,11 @@
+#' The miloR package
+#'
+#' The \pkg{miloR} package provides modular functions to perform differential
+#' abundance testing on replicated single-cell experiments. For details please
+#' see the vignettes \code{vignette("milo_demo", package="miloR")} and
+#' \code{vignette("milo_gastrulation", package="miloR")}.
+#'
+#' @author Mike Morgan & Emma Dann
+#'
+#' @name miloR-package
+NULL
