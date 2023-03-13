09165370059fe963e8cd34687979ba0de3cb9c87
MikeDMorgan <michael.morgan@abdn.ac.uk>
2023-03-13 12:23:53 +0000

    allow nhood subsetting in annotateNhoods

--- R/annotateNhoods.R
+++ R/annotateNhoods.R
@@ -13,6 +13,11 @@
 #' \code{testNhoods}.
 #' @param coldata_col A character scalar determining which column of \code{colData(x)} stores
 #' the annotation to be added to the neighbourhoods
+#' @param subset.nhoods A character, numeric or logical vector that will subset the annotation to the specific nhoods. If
+#' a character vector these should correspond to row names of \code{nhoodCounts}. If a logical vector then
+#' these should have the same \code{length} as \code{nrow} of \code{nhoodCounts}. If numeric, then these are assumed
+#' to correspond to indices of \code{nhoodCounts} - if the maximal index is greater than \code{nrow(nhoodCounts(x))}
+#' an error will be produced. This is necessary if \code{testNhoods} was run using \code{subset.nhoods=...}.
 #'
 #' @details
 #' For each neighbourhood, this calculates the most frequent value of \code{colData(x)[coldata_col]}
@@ -34,19 +39,23 @@
 #'
 #' @export
 #' @rdname annotateNhoods
-annotateNhoods <- function(x, da.res, coldata_col){
-  if(!is(x, "Milo")){
-    stop("Unrecognised input type - must be of class Milo")
-  }
+annotateNhoods <- function(x, da.res, coldata_col, subset.nhoods=NULL){
+    if(!is(x, "Milo")){
+        stop("Unrecognised input type - must be of class Milo")
+    }
 
-  if(!coldata_col %in% names(colData(x))){
-    stop(coldata_col, " is not a column in colData(x)")
-  }
+    if(!coldata_col %in% names(colData(x))){
+        stop(coldata_col, " is not a column in colData(x)")
+    }
 
-  if(ncol(nhoods(x)) != nrow(da.res)){
-    stop("the number of rows in da.res does not match the number of neighbourhoods in nhoods(x). Are you sure da.res is the output of testNhoods(x)?")
-  }
-
+    if(is.null(subset.nhoods)){
+        if(ncol(nhoods(x)) != nrow(da.res)){
+            stop("the number of rows in da.res does not match the number of neighbourhoods in nhoods(x). Are you sure da.res is the output of testNhoods(x) or did you use subset.nhoods?")
+        }
+        keep.nh <- rep(TRUE, ncols(nhoods(x)))
+    } else{
+        keep.nh <- subset.nhoods
+    }
 
   anno_vec <- colData(x)[[coldata_col]]
   if (!is.factor(anno_vec)) {
@@ -56,14 +65,14 @@ annotateNhoods <- function(x, da.res, coldata_col){
 
   ## Count occurrence of labels in each nhood
   n.levels <- length(levels(anno_vec))
-  nhood_counts <- vapply(seq_len(ncol(nhoods(x))), FUN=function(n) table(anno_vec[which(nhoods(x)[,n]==1)]),
+  nhood_counts <- vapply(seq_len(ncol(nhoods(x)[, keep.nh, drop=FALSE])), FUN=function(n) table(anno_vec[which(nhoods(x)[ ,n]==1)]),
                          FUN.VALUE=numeric(n.levels))
   nhood_counts <- t(nhood_counts)
-  rownames(nhood_counts) <- seq_len(ncol(nhoods(x)))
+  rownames(nhood_counts) <- seq_len(ncol(nhoods(x)[, keep.nh, drop=FALSE]))
 
   ## Fetch the most frequent label
-  max_val <- apply(nhood_counts, 1, function(x) colnames(nhood_counts)[which.max(x)])
-  max_frac <- apply(nhood_counts, 1, function(x) max(x)/sum(x))
+  max_val <- apply(nhood_counts, 1, function(X) colnames(nhood_counts)[which.max(X)])
+  max_frac <- apply(nhood_counts, 1, function(X) max(X)/sum(X))
 
   ## Add to da.res table
   da.res[coldata_col] <- max_val
